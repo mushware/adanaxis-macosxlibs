afadef51b36eb56cce3a948c0253d315f5839b21
Andy Southgate <andy@mushware.com>
2002-09-07 11:39:04 +0000

    Linrary updates

---
+++ MakeLinks.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+##############################################################################
+#
+# This file contains original work by Andy Southgate.  Contact details can be
+# found at http://www.mushware.co.uk.  This file was placed in the Public
+# Domain by Andy Southgate and Mushware Limited in 2002.
+#
+# This software carries NO WARRANTY of any kind.
+#
+##############################################################################
+
+#
+# $Id$
+# $Log$
+#
+
+# Remake the links that CVS loses
+
+ln -s libSDL-1.2.0.0.5.dylib libSDL-1.2.0.dylib
+ln -s libSDL_mixer-1.2.0.2.3.dylib libSDL_mixer-1.2.0.dylib
+ln -s libogg.0.3.0.dylib libogg.0.dylib
+ln -s libsmpeg-0.4.0.1.3.dylib libsmpeg-0.4.0.dylib
+ln -s libvorbis.0.1.0.dylib libvorbis.0.dylib
+ln -s libvorbisenc.0.1.0.dylib libvorbisenc.0.dylib
+ln -s libvorbisfile.0.2.0.dylib libvorbisfile.0.dylib
