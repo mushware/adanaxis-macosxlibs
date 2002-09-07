03d5b288a3657e2779d2364abc2f76283c0e1a55
Andy Southgate <andy@mushware.com>
2002-09-07 12:57:28 +0000

    Building on MacOS X 10.2 with new libraries

--- MakeLinks.sh
+++ MakeLinks.sh
@@ -11,8 +11,11 @@
 ##############################################################################
 
 #
-# $Id: MakeLinks.sh,v 1.1 2002/07/01 13:42:59 southa Exp $
+# $Id: MakeLinks.sh,v 1.1.4.1 2002/09/07 11:43:09 southa Exp $
 # $Log: MakeLinks.sh,v $
+# Revision 1.1.4.1  2002/09/07 11:43:09  southa
+# Library updates
+#
 # Revision 1.1  2002/07/01 13:42:59  southa
 # MacOS X installer automation
 #
@@ -20,10 +23,12 @@
 
 # Remake the links that CVS loses
 
+ln -s libexpat.0.3.0.dylib libexpat.0.dylib
 ln -s libSDL-1.2.0.0.5.dylib libSDL-1.2.0.dylib
 ln -s libSDL_mixer-1.2.0.2.2.dylib libSDL_mixer-1.2.0.dylib
 ln -s libogg.0.4.0.dylib libogg.0.dylib
 ln -s libsmpeg-0.4.0.1.3.dylib libsmpeg-0.4.0.dylib
+ln -s libungif.4.1.0.dylib libungif.4.dylib
 ln -s libvorbis.0.2.0.dylib libvorbis.0.dylib
-ln -s libvorbisenc.2.0.0.dylib libvorbisenc.0.dylib
-ln -s libvorbisfile.3.0.0.dylib libvorbisfile.0.dylib
+ln -s libvorbisenc.2.0.0.dylib libvorbisenc.2.dylib
+ln -s libvorbisfile.3.0.0.dylib libvorbisfile.3.dylib
