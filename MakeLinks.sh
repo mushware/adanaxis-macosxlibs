a823efa6b3525a4fa54599ba9ef3bf76371c854b
Andy Southgate <andy@mushware.com>
2003-12-28 17:37:09 +0000

    Rebuilt libraries for gcc 3.3 and removed unnecessary

--- MakeLinks.sh
+++ MakeLinks.sh
@@ -11,8 +11,11 @@
 ##############################################################################
 
 #
-# $Id: MakeLinks.sh,v 1.2 2002/10/20 11:13:27 southa Exp $
+# $Id: MakeLinks.sh,v 1.3 2002/11/20 11:38:30 southa Exp $
 # $Log: MakeLinks.sh,v $
+# Revision 1.3  2002/11/20 11:38:30  southa
+# Fixed load paths to be executable-relative
+#
 # Revision 1.2  2002/10/20 11:13:27  southa
 # New library builds for 0.0.6
 #
@@ -23,13 +26,9 @@
 
 # Remake the links that CVS loses
 
-ln -s libSDL-1.2.0.0.5.dylib libSDL-1.2.0.dylib
-ln -s libSDL_mixer-1.2.0.2.2.dylib libSDL_mixer-1.2.0.dylib
-ln -s libSDL_net-1.2.0.0.4.dylib libSDL_net-1.2.0.dylib
-ln -s libexpat.0.3.0.dylib libexpat.0.dylib
-ln -s libogg.0.4.0.dylib libogg.0.dylib
+ln -s libtiff.3.6.1.dylib libtiff.dylib
+ln -s libSDL-1.2.0.0.6.dylib libSDL-1.2.0.dylib
+ln -s libSDL_mixer-1.2.0.2.3.dylib libSDL_mixer-1.2.0.dylib
 ln -s libsmpeg-0.4.0.1.3.dylib libsmpeg-0.4.0.dylib
-ln -s libvorbis.0.2.0.dylib libvorbis.0.dylib
-ln -s libvorbisfile.3.0.0.dylib libvorbisfile.3.dylib
 
 
